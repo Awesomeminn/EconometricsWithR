23c21e818f32f23e535709e6ddfbff7332bdb7fa
Minjae Kim <pietromin98j@naver.com>
2024-06-27 14:19:34 +0900

    First upload the currently working files.

---
+++ 1.Package Setting.R
@@ -0,0 +1,9 @@
+# 패키지 설치여부 확인 "패키지명"%in% installed.packages()
+
+"Ecdat"%in%installed.packages()
+"AER"%in%installed.packages()
+"lmtest"%in%installed.packages()
+"car"%in%installed.packages()
+"sandwich"%in%installed.packages()
+"wooldridge"%in%installed.packages()
+"loedata"%in%installed.packages()
